23d580210967f6c2f445d26dddab23e294f37dc2
张瑞 <zhangrui@RedQueen.local>
2013-05-23 22:57:40 +0800

    Initial Commit

---
+++ 20130523Demo/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  20130523Demo
+//
+//  Created by 张瑞 on 13-5-23.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ 20130523Demo/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  20130523Demo
+//
+//  Created by 张瑞 on 13-5-23.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
